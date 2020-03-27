80ca2cd5f4451066099c47eee185de3f1eef85d1
EddieOrozco <orozcoe230@gmail.com>
2020-03-27 11:20:05 -0500

    Init commit / How to create reusable func components with props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Im hungry."
+        />
+      </ApprovalCard>
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Jane"
+        timeAgo="Today at 6:00PM"
+        content="I play video games."
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Eddie"
+        timeAgo="Today at 9:45AM"
+        content="Nice!"
+      />
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
